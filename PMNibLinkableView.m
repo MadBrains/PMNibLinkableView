2918fadd02ad142ceb32e55da325e28638e822c9
Antol <antol.peshkov@gmail.com>
2016-07-26 11:14:00 +0100

    Fix issue with parallel calling of swizling

--- PMNibLinkableView.m
+++ PMNibLinkableView.m
@@ -11,19 +11,47 @@
 
 @interface PMNibLinkableView ()
 @property (nonatomic, assign) BOOL isAwake;
+@property void awakeFromLinkableNib;
 @end
 
 @implementation PMNibLinkableView
+@dynamic awakeFromLinkableNib;
 
 static int kEJNibLinkableViewTag = 999;
+
++ (void)initialize {
+    [super initialize];
+    if ([self class] != [PMNibLinkableView class]) {
+        [self swizzleAwakeFromNib];
+    }
+}
+
++ (void)swizzleAwakeFromNib
+{
+    Class class = [self class];
+    SEL originalSelector = @selector(awakeFromNib);
+    SEL swizzledSelector = @selector(awakeFromLinkableNib);
+    
+    class_addMethod(class, swizzledSelector, (IMP)awakeFromLinkableNib, "v@:");
+    
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    method_exchangeImplementations(originalMethod, swizzledMethod);
+}
+
+void awakeFromLinkableNib(PMNibLinkableView *self, SEL _cmd) {
+    if (!self.isAwake) {
+        self.isAwake = YES;
+        [self awakeFromLinkableNib];
+    }
+}
 
 - (id)awakeAfterUsingCoder:(NSCoder *)aDecoder
 {
     if (self.subviews.count != 0 && self.tag != kEJNibLinkableViewTag) {
         return [super awakeAfterUsingCoder:aDecoder];
     }
-    
-    [self swizzleAwakeFromNib];
     
     UIView *loadedView = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:nil options:nil] firstObject];
     loadedView.frame = self.frame;
@@ -53,41 +81,5 @@ static int kEJNibLinkableViewTag = 999;
     return loadedView;
 }
 
-- (void)swizzleAwakeFromNib
-{
-    Class class = [self class];
-    
-    SEL originalSelector = @selector(awakeFromNib);
-    SEL swizzledSelector = @selector(awakeFromLinkableNib);
-    
-    Method originalMethod = class_getInstanceMethod(class, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-    
-    BOOL didAddMethod = class_addMethod(class,
-                                        originalSelector,
-                                        method_getImplementation(swizzledMethod),
-                                        method_getTypeEncoding(swizzledMethod));
-    if (didAddMethod) {
-        class_replaceMethod(class,
-                            swizzledSelector,
-                            method_getImplementation(originalMethod),
-                            method_getTypeEncoding(originalMethod));
-    }
-    else {
-        method_exchangeImplementations(originalMethod, swizzledMethod);
-    }
-}
-
-- (void)awakeFromLinkableNib {
-    if (self.isAwake) {
-        [self swizzleAwakeFromNib];
-    }
-    else {
-        self.isAwake = YES;
-        [self awakeFromLinkableNib];
-    }
-}
-
 @end
-
 
