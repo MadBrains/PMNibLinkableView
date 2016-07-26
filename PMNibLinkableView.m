657ecc77bd106b45c7bbb8e03c0ea8bc3f10c841
Antol <antol.peshkov@gmail.com>
2016-07-26 15:00:34 +0100

    Fix magic for unimplemented awakeFromNib in child view

--- PMNibLinkableView.m
+++ PMNibLinkableView.m
@@ -37,7 +37,19 @@ static int kEJNibLinkableViewTag = 999;
     Method originalMethod = class_getInstanceMethod(class, originalSelector);
     Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
     
-    method_exchangeImplementations(originalMethod, swizzledMethod);
+    BOOL didAddMethod = class_addMethod(class,
+                                        originalSelector,
+                                        method_getImplementation(swizzledMethod),
+                                        method_getTypeEncoding(swizzledMethod));
+    if (didAddMethod) {
+        class_replaceMethod(class,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+    }
+    else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
 }
 
 void awakeFromLinkableNib(PMNibLinkableView *self, SEL _cmd) {
